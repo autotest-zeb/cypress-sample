1d45d0a5dcd4d8d6488f23e0a0c7ba5a99a3602c
obabich-solvd <71430326+obabich-solvd@users.noreply.github.com>
2023-09-25 19:24:07 +0300

    to cypress 12

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: '@zebrunner/javascript-agent-cypress',
+  reporterOptions: {
+    reportingServerHostname: 'https://<workspace>.zebrunner.com/',
+    reportingServerAccessToken: '<accessToken>',
+    reportingProjectKey: '<project_key>',
+    reportingRunEnvironment: 'DEMO',
+    reportingRunBuild: '2.41.2.2431-SNAPSHOT',
+    reportingRunDisplayName: 'Cypress Demo Launch',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})
