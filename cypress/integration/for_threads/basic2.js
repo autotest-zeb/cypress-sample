cb56ee827d2dd22cd4e6c24ab98a6e3d4c491e83
obabich-solvd <71430326+obabich-solvd@users.noreply.github.com>
2023-09-26 11:20:16 +0300

    added tests

---
+++ cypress/integration/for_threads/basic2.js
@@ -0,0 +1,26 @@
+describe('2. Basic reporting test', () => {
+  const url = 'https://www.google.com/';
+
+  it('Google search should be passed', () => {
+    const searchValue = 'Zebrunner';
+    performGoogleSearch(searchValue);
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+  });
+
+  it('Google search should be failed', () => {
+    const searchValue = 'Cypress';
+    performGoogleSearch(searchValue);
+
+    console.log(`Verify first search result equals to search value`);
+    cy.xpath("//*[@id='search']//a").should('have.text', searchValue);
+  });
+
+  function performGoogleSearch(searchValue) {
+    cy.visit(url).contains('Google');
+
+    console.log(`Performing search with value Zebrunner`);
+    cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
+  }
+});
